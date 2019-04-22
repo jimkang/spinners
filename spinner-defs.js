3209a23b498af54e0217fb7edc22a40e6cd0beb7
Jim Kang <jimkang@gmail.com>
2019-04-22 10:02:23 -0400

    Generate spinner defs with tablenest.

--- spinner-defs.js
+++ spinner-defs.js
@@ -1,19 +1,36 @@
-// Note: randomId does not respect the seed that probable uses.
-var randomId = require('idmaker').randomId;
+var { Tablenest, r, s, f } = require('tablenest');
 
-function SpinnerDefs({ probable }) {
+var idChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+
+function SpinnerDefs({ random }) {
+  var tablenest = Tablenest({ random });
+
+  var literalSpinner = r({
+    id: s`spinner-{idChars}`,
+    style: 'spinner',
+    image: r`literalSpinnerImages`,
+    r: f((result, p) => 5 + p.roll(20)),
+    duration: f((result, p) => `${0.2 + p.roll(10)}s`),
+    // TODO: Clean this up.
+    idChars: f(
+      (result, p) =>
+        p.pickFromArray(idChars) +
+        p.pickFromArray(idChars) +
+        p.pickFromArray(idChars) +
+        p.pickFromArray(idChars)
+    )
+  });
+
+  var literalSpinnerImages = [
+    [2, 'media/Yellow_Fidget_Spinner.png'],
+    [1, 'media/fidget-spinner-2452983_1280.png']
+  ];
+
   return {
-    default: {
-      roll() {
-        return {
-          id: 'spinner-' + randomId(4),
-          style: 'spinner',
-          image: 'media/Yellow_Fidget_Spinner.png',
-          r: 5 + probable.roll(20),
-          duration: `${0.2 + probable.roll(10)}s`
-        };
-      }
-    }
+    default: tablenest({
+      root: [[1, literalSpinner]],
+      literalSpinnerImages
+    })
   };
 }
 
