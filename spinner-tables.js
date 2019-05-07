9946179c3c577f3cd09dee95fb78ab64daf4d3d7
Jim Kang <jimkang@gmail.com>
2019-05-06 21:08:19 -0400

    Refactor. Have fewer speeds.

--- spinner-tables.js
+++ spinner-tables.js
@@ -2,24 +2,19 @@ var { Tablenest, r, f, d } = require('tablenest');
 var images = require('./images');
 var RandomId = require('@jimkang/randomid');
 
-var imageTables = {
-  literalSpinner: [
-    [2, images.redFidgetSpinner],
-    [1, images.yellowFidgetSpinner]
-  ],
-  ammonite: [[3, images.ammonite], [2, images.inkAmmonite]],
-  clockFace: images.officeClockFace,
-  clockHourHand: images.officeClockHourHand,
-  clockMinuteHand: images.officeClockMinuteHand,
-  cat: [
-    [32, images.bonusMorphBall],
-    [32, images.bonusMorphBall2],
-    [1, images.bonusMorphBall3],
-    [28, images.wilyMorphBall],
-    [32, images.wilyMorphBall2]
-  ],
-  pizza: images.pizza
-};
+// The duration of the rotation will be 1/speed seconds.
+// e.g. speed 60 => 1/60 s duration. speed 0.5 => 2s duration.
+// speed 5 => 0.2 s duration
+var speedTable = [
+  [1, d`d4x0.2`],
+  [1, d`d4x0.4`],
+  [1, d`d4x0.6`],
+  //[1, d`d10x0.2`],
+  //[1, d`d10x0.4`],
+  //[1, d`d10x0.6`],
+  //[1, d`d10x0.8`],
+  [1, d`d10`]
+];
 
 function SpinnerTables({ random }) {
   var tablenest = Tablenest({ random });
@@ -27,67 +22,55 @@ function SpinnerTables({ random }) {
 
   var getId = f(() => 'spinner-' + randomId(4));
 
-  var spinnerSubTables = {
-    literalSpinner: r({
-      id: getId,
-      image: r`literalSpinnerImages`,
-      r: f((result, p) => 5 + p.roll(20)),
-      // The duration of the rotation will be 1/speed seconds.
-      // e.g. speed 60 => 1/60 s duration. speed 0.5 => 2s duration.
-      // speed 5 => 0.2 s duration
-      speed: d`d50x0.1`,
-      duration: f((result, p) => `${0.2 + p.roll(10)}s`)
+  return {
+    literalSpinner: makeSpinnerTable({
+      radius: d`d20+4`,
+      images: [[2, images.redFidgetSpinner], [1, images.yellowFidgetSpinner]]
     }),
-    ammonite: r({
-      id: getId,
-      image: r`ammoniteImages`,
-      r: f((result, p) => 10 + p.roll(15)),
-      duration: f((result, p) => `${p.rollDie(50) / 5}s`)
+    ammonite: makeSpinnerTable({
+      radius: d`d15+9`,
+      images: [[3, images.ammonite], [2, images.inkAmmonite]]
     }),
-    clockFace: r({
-      id: getId,
-      image: r`clockFaceImages`,
-      r: d`d6+5`,
-      duration: d`d50+0.2`
+    clockFace: makeSpinnerTable({
+      radius: d`d6+5`,
+      images: images.officeClockFace
     }),
-    clockHourHand: r({
-      id: getId,
-      image: r`clockHourHandImages`,
-      r: d`d6+5`,
-      duration: d`2d10`
+    clockHourHand: makeSpinnerTable({
+      radius: d`d6+5`,
+      images: images.officeClockHourHand
     }),
-    clockMinuteHand: r({
-      id: getId,
-      image: r`clockMinuteHandImages`,
-      r: d`d6+5`,
-      duration: f((o, p) => p.rollDie(20) / 10) // TODO: Make this work in dicecup: d`d6/10`
+    clockMinuteHand: makeSpinnerTable({
+      radius: d`d6+5`,
+      images: images.officeClockMinuteHand
     }),
-    cat: r({
-      id: getId,
-      image: r`catImages`,
-      r: d`d6+5`,
-      duration: f((o, p) => `${5 + p.rollDie(25) / 5}s`)
+    cat: makeSpinnerTable({
+      radius: d`d6+5`,
+      images: [
+        [32, images.bonusMorphBall],
+        [32, images.bonusMorphBall2],
+        [1, images.bonusMorphBall3],
+        [28, images.wilyMorphBall],
+        [32, images.wilyMorphBall2]
+      ]
     }),
-    pizza: r({
-      id: getId,
-      image: r`pizzaImages`,
-      r: d`d20+5`,
-      duration: f((o, p) => `${0.2 + p.roll(10)}s`)
+    pizza: makeSpinnerTable({
+      radius: d`d20+5`,
+      images: images.pizza
     })
   };
 
-  return makeTables({ spinnerSubTables, imageTables, tablenest });
-}
-
-function makeTables({ spinnerSubTables, imageTables, tablenest }) {
-  var tables = {};
-  for (var key in spinnerSubTables) {
-    tables[key] = tablenest({
-      root: spinnerSubTables[key],
-      [key + 'Images']: imageTables[key]
+  function makeSpinnerTable({ radius, images }) {
+    return tablenest({
+      root: r({
+        id: getId,
+        image: r`images`,
+        r: radius,
+        speed: r`speed`
+      }),
+      images,
+      speed: speedTable
     });
   }
-  return tables;
 }
 
 module.exports = SpinnerTables;
