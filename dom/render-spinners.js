5625204cf81ad6713d2abcd4f9b050692e62b2a6
Jim Kang <jimkang@gmail.com>
2019-05-16 19:52:42 -0400

    Factor out orbit stuff.

--- dom/render-spinners.js
+++ dom/render-spinners.js
@@ -3,6 +3,7 @@ var accessor = require('accessor');
 var pathExists = require('object-path-exists');
 var renderLayers = require('./render-layers');
 var curry = require('lodash.curry');
+var { makeOrbitForSpinner, getOrbitIdForSpinner } = require('./orbit');
 
 var board = d3.select('#board');
 var orbitPathRoot = board.select('#orbit-paths');
@@ -185,108 +186,6 @@ function addRotationTransform({ spinnersSel, className, type = 'rotate' }) {
       .attr('repeatCount', 'indefinite')
       .classed(className, true)
   );
-}
-
-function getOrbitIdForSpinner(spinner) {
-  return `orbit-${spinner.data.id}`;
-}
-
-function makeOrbitForSpinner(
-  { clockwise, cx, cy },
-  spinner,
-  orbitIndex,
-  spinners
-) {
-  return {
-    id: getOrbitIdForSpinner(spinner),
-    d: makePathDataForOrbit(clockwise, cx, cy, spinner, orbitIndex, spinners)
-  };
-}
-
-function makePathDataForOrbit(
-  clockwise,
-  cx,
-  cy,
-  spinner,
-  orbitIndex,
-  spinners
-) {
-  var orbitR = spinner.orbitR;
-  var startAngle = ((2 * Math.PI) / spinners.length) * orbitIndex;
-  var startPoint = positionOnCircle(cx, cy, startAngle, orbitR);
-
-  // var debugColor = ['red', 'orange', 'yellow', 'green', 'blue'][orbitIndex];
-  // this.addDebugCircleD3(startPoint, debugColor);
-
-  var rx = orbitR;
-  var ry = orbitR;
-  var xAxisRotation = 0;
-  var largeArcFlag = 1;
-  var sweepFlag = clockwise ? 1 : 0;
-
-  var vectorToOpposite = {
-    x: -2 * (startPoint.x - cx),
-    y: -2 * (startPoint.y - cy)
-  };
-
-  // this.addDebugCircleD3({
-  //   x: startPoint.x + vectorToOpposite.x,
-  //   y: startPoint.y + vectorToOpposite.y
-  // },
-  // debugColor);
-
-  // Example data path:
-  // <path d="M0,400
-  //          a200,200 0 1 0 400,0
-  //          a200,200 0 1 0 -400,0"
-  //       fill="green" stroke="blue" stroke-width="5" id="redOrbit" />
-
-  var path =
-    // Move command
-    'M' +
-    startPoint.x +
-    ',' +
-    startPoint.y +
-    ' ' +
-    // First arc command
-    'a' +
-    rx +
-    ',' +
-    ry +
-    ' ' +
-    xAxisRotation +
-    ' ' +
-    largeArcFlag +
-    ' ' +
-    sweepFlag +
-    ' ' +
-    vectorToOpposite.x +
-    ',' +
-    vectorToOpposite.y +
-    ' ' +
-    // Second arc command
-    'a' +
-    rx +
-    ',' +
-    ry +
-    ' ' +
-    xAxisRotation +
-    ' ' +
-    largeArcFlag +
-    ' ' +
-    sweepFlag +
-    ' ' +
-    -vectorToOpposite.x +
-    ',' +
-    -vectorToOpposite.y;
-
-  return path;
-}
-
-function positionOnCircle(cx, cy, angle, radius) {
-  var y = radius * Math.sin(angle);
-  var x = radius * Math.cos(angle);
-  return { x: cx + x, y: cy + y };
 }
 
 module.exports = renderSpinners;
