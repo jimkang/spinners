b03099728022eddf7cef62df4dfc835e39c5f01e
Jim Kang <jimkang@gmail.com>
2019-07-08 21:55:49 -0400

    Make sure #board takes up 100% of window so that squared board is as large as possible. Frame #board to avoid breaking illusion when things go over the edge of the board. Flexbox wrap the spinventory.

--- dom/squarify-board.js
+++
@@ -1,9 +0,0 @@
-var d3 = require('d3-selection');
-var board = d3.select('#board');
-
-function squarifyBoard() {
-  var boardWidth = board.node().getBoundingClientRect().width;
-  board.attr('height', boardWidth);
-}
-
-module.exports = squarifyBoard;
